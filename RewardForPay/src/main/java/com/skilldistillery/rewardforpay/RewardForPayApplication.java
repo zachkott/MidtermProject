77336deed32c54e193794a570f9a0bde82b48631
zachkott <zachkott@gmail.com>
2022-08-18 10:58:59 -0700

    Create Boot MVC Project, link JPA project dependency

---
+++ RewardForPay/src/main/java/com/skilldistillery/rewardforpay/RewardForPayApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rewardforpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardForPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardForPayApplication.class, args);
+	}
+
+}
---
+++ RewardForPay/src/test/java/com/skilldistillery/rewardforpay/RewardForPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rewardforpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardForPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
