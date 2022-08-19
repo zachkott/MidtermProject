0fdea220544da9b68cf237e5d3e766c487b1dc25
Jamie Patterson <jamie.pattersonre@gmail.com>
2022-08-19 13:19:31 -0600

    Updated tests

--- JPARewardForPay/src/test/java/com/skilldistillery/rewardforpay/entities/StatusTest.java
+++ JPARewardForPay/src/test/java/com/skilldistillery/rewardforpay/entities/StatusTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.rewardforpay.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,42 +14,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StatusTest {
-	static EntityManagerFactory emf;
-	EntityManager em;
-	private Status status;
-	
-	@BeforeAll
-	static void setUpTest() {
-		emf = Persistence.createEntityManagerFactory("JPARewardForPay");
-	}
-	
-	@AfterAll
-	static void tearDownTest() {
-		emf.close();
-	}
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		em = emf.createEntityManager();
-		status = em.find(Status.class, 1);
-	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-		status = null;
-		em.close();
-	}
+		static EntityManagerFactory emf;
+		EntityManager em;
+		private Status status;
+		
+		@BeforeAll
+		static void setUpTest() {
+			emf = Persistence.createEntityManagerFactory("JPARewardForPay");
+		}
+		
+		@AfterAll
+		static void tearDownTest() {
+			emf.close();
+		}
+		
+		@BeforeEach
+		void setUp() throws Exception {
+			em = emf.createEntityManager();
+			status = em.find(Status.class, 1);
+		}
 
-	@Test
-	void test_Status_creates_new_actor() {
-		assertNotNull(status);
-		assertEquals("Approved", status.getName());
-	}
-	
-	@Test
-	void test_Status_to_Prize_mapping() {
-		assertNotNull(status);
-		assertEquals("MousePad", status.getPrizes().get(0).getName());
-	}
+		@AfterEach
+		void tearDown() throws Exception {
+			status = null;
+			em.close();
+		}
+
+		@Test
+		void test_Status_creates_new_actor() {
+			assertNotNull(status);
+			assertEquals("Approved", status.getName());
+		}
+		
+		@Test
+		void test_Status_to_Prize_mapping() {
+			assertNotNull(status);
+			assertEquals("MousePad", status.getPrizes().get(0).getName());
+		}
 
 }
--- JPARewardForPay/src/test/java/com/skilldistillery/rewardforpay/entities/TierTest.java
+++ JPARewardForPay/src/test/java/com/skilldistillery/rewardforpay/entities/TierTest.java
@@ -1,7 +1,6 @@
 package com.skilldistillery.rewardforpay.entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,44 +10,45 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+class TierTest {
+	
+		static EntityManagerFactory emf;
+		EntityManager em;
+		private Tier tier;
+		
+		@BeforeAll
+		static void setUpTest() {
+			emf = Persistence.createEntityManagerFactory("JPARewardForPay");
+		}
+		
+		@AfterAll
+		static void tearDownTest() {
+			emf.close();
+		}
+		
+		@BeforeEach
+		void setUp() throws Exception {
+			em = emf.createEntityManager();
+			tier = em.find(Tier.class, 1);
+		}
 
-class Test {
-	static EntityManagerFactory emf;
-	EntityManager em;
-	private Tier tier;
-	
-	@BeforeAll
-	static void setUpTest() {
-		emf = Persistence.createEntityManagerFactory("JPARewardForPay");
-	}
-	
-	@AfterAll
-	static void tearDownTest() {
-		emf.close();
-	}
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		em = emf.createEntityManager();
-		tier = em.find(Tier.class, 1);
-	}
+		@AfterEach
+		void tearDown() throws Exception {
+			tier = null;
+			em.close();
+		}
 
-	@AfterEach
-	void tearDown() throws Exception {
-		tier = null;
-		em.close();
-	}
-
-	@Test
-	void test_Tier_creates_new_tier() {
-		assertNotNull(tier);
-		assertEquals("Copper", tier.getName());
-	}
-	
-	@Test
-	void test_Tier_to_Prize_mapping() {
-		assertNotNull(tier);
-		assertEquals("Mouse Pad", tier.getPrizes().get(0).getName());
-	}
+		@Test
+		void test_Tier_creates_new_tier() {
+			assertNotNull(tier);
+			assertEquals("Copper", tier.getName());
+		}
+		
+		@Test
+		void test_Tier_to_Prize_mapping() {
+			assertNotNull(tier);
+			assertEquals("Mouse Pad", tier.getPrizes().get(0).getName());
+		}
 }
