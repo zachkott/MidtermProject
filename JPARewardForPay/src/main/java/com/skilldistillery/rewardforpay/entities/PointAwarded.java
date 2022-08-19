bffc1728c78dac147f78422d9b8842a309ebd172
Daniel Schulenberg <dschumil@gmail.com>
2022-08-19 11:06:38 -0700

    PointAwared/junits

--- JPARewardForPay/src/main/java/com/skilldistillery/rewardforpay/entities/PointAwarded.java
+++ JPARewardForPay/src/main/java/com/skilldistillery/rewardforpay/entities/PointAwarded.java
@@ -1,8 +1,8 @@
 package com.skilldistillery.rewardforpay.entities;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,18 +22,110 @@ public class PointAwarded {
 	
 	@ManyToOne
 	@JoinColumn(name="employee_id")
-	private int employeeId;
+	private Employee employee;
 		
 	private String description;
 	
 	@ManyToOne
 	@JoinColumn(name="award_status_id")
-	private int award_status_id;
+	private Status status;
 	
+	private boolean judgement;
 	
-
+	@ManyToOne
+	@JoinColumn(name="requested_id")
+	private User user;
+	
 	public PointAwarded() {
 		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getIssued() {
+		return issued;
+	}
+
+	public void setIssued(LocalDateTime issued) {
+		this.issued = issued;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public boolean isJudgement() {
+		return judgement;
+	}
+
+	public void setJudgement(boolean judgement) {
+		this.judgement = judgement;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "PointAwarded [id=" + id + ", amount=" + amount + ", issued=" + issued + ", employee=" + employee
+				+ ", description=" + description + ", status=" + status + ", judgement=" + judgement + ", user=" + user
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PointAwarded other = (PointAwarded) obj;
+		return id == other.id;
 	}
 
 	
