f70a806111df041ce73f28e8236baea860d6997b
Daniel Schulenberg <dschumil@gmail.com>
2022-08-20 10:08:29 -0700

    starting user account info

--- RewardForPay/src/main/java/com/skilldistillery/rewardforpay/controllers/AccountController.java
+++ RewardForPay/src/main/java/com/skilldistillery/rewardforpay/controllers/AccountController.java
@@ -1,22 +1,33 @@
 package com.skilldistillery.rewardforpay.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.rewardforpay.data.UserDAO;
+import com.skilldistillery.rewardforpay.entities.Employee;
+import com.skilldistillery.rewardforpay.entities.User;
 
 @Controller
 public class AccountController {
-	
+
 	@Autowired
 	private UserDAO userDao;
 
-	@RequestMapping(path= {"account.do"})
-	public String home(Model model) {
-		model.addAttribute("SMOKETEST", userDao.findById(1)); //DELETE
-		return "account";
-		
+	@RequestMapping(path = { "account.do" })
+	public String home(HttpSession session, Model model) {
+		User user = (User) session.getAttribute("loggedInUser");
+		if (user != null) {
+			Employee employee = user.getEmployeeId();
+			model.addAttribute("employee",employee);
+			model.addAttribute("awardbalance",userDao.findPointBalance(employee.getId()));
+			return "account";
+		} else {
+			return "login";
+		}
+
 	}
 }
--- RewardForPay/src/main/webapp/WEB-INF/account.jsp
+++ RewardForPay/src/main/webapp/WEB-INF/account.jsp
@@ -10,6 +10,29 @@
 <%@ include file="nav.jsp" %>
 <h1>Im the Account Page</h1>
 
+ <c:choose>
+   <c:when test="${! empty sessionScope.loggedInUser}">
+   <h2>Welcome, ${employee.firstName} ${employee.lastName}</h2>
+   		<br>
+
+				<h5>What would you like to do?</h5>
+				<br>
+	<ul>
+					<li><a href="machineSearch.do" class="title">Update information</a></li>
+					<br>
+					<li><a href="machineSearch.do" class="title"> Create an award</a></li>
+					<br>
+					<li><a href="addMaintenance.do" class="title"> Create an employee</a></li>
+					<br>
+					<li> <a href="updateMaintenance.do" class="title"> Update/Delete a Maintenance Project</a></li>
+</ul>
+   </c:when>
+   <c:otherwise>
+     <h2>Not logged in.</h2>
+     <a href="login.do">Please log in to access your account information.</a>
+   </c:otherwise>
+ </c:choose>
+ 
 
 </body>
 </html>
